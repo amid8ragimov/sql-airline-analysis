8e9576d8783d82844013b1b1f64d15de6411bdfb
amid8ragimov <amid8ragimov@gmail.com>
2026-03-17 00:18:16 +0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -5,3 +5,19 @@ FROM Passenger p
 JOIN Pass_in_trip pit ON p.id = pit.passenger
 GROUP BY p.name
 ORDER BY flights DESC;
+
+-- Самые популярные направления
+
+SELECT town_to, COUNT(*) AS flights
+FROM Trip
+JOIN Pass_in_trip pit ON Trip.id = pit.trip
+GROUP BY town_to
+ORDER BY flights DESC;
+
+-- Пассажиры которые летали больше 1 раза
+
+SELECT p.name, COUNT(*) AS flights
+FROM Passenger p
+JOIN Pass_in_trip pit ON p.id = pit.passenger
+GROUP BY p.name
+HAVING COUNT(*) > 1;
